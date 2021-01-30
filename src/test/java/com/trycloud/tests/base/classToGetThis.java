7f6318e93913675cf01c151e51caf25d57d403a4
Olesea1986 <olesea86williams@gmail.com>
2021-01-29 20:43:28 -0600

    commit

--- src/test/java/com/trycloud/tests/base/classToGetThis.java
+++ src/test/java/com/trycloud/tests/base/classToGetThis.java
@@ -4,5 +4,7 @@ public class classToGetThis {
     public static void main(String[] args) {
         System.out.println("woouuuuuuuurk");
         System.out.println("change");
+        // almost done
+
     }
 }
