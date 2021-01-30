1abf4acecca8965cf1a41e14a25cdd42c514023e
Ahmed Sdiri <sdiri.ad@gmail.com>
2021-01-29 20:33:34 -0600

    change in the classToGetThis

--- src/test/java/com/trycloud/tests/base/classToGetThis.java
+++ src/test/java/com/trycloud/tests/base/classToGetThis.java
@@ -3,5 +3,6 @@ package com.trycloud.tests.base;
 public class classToGetThis {
     public static void main(String[] args) {
         System.out.println("woouuuuuuuurk");
+        System.out.println("change");
     }
 }
