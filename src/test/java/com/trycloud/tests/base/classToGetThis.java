78d86bab46179b6ab738d1bec9374bc8661a5bcd
Anna Shabalina <shabalina@gmail.com>
2021-01-29 21:44:07 -0500

    Anna`s commit.

--- src/test/java/com/trycloud/tests/base/classToGetThis.java
+++ src/test/java/com/trycloud/tests/base/classToGetThis.java
@@ -4,5 +4,6 @@ public class classToGetThis {
     public static void main(String[] args) {
         System.out.println("woouuuuuuuurk");
         System.out.println("change");
+        // new changes are added
     }
 }
