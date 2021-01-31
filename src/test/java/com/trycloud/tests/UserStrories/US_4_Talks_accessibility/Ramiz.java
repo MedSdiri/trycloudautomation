744c882d8539decf7287fb1984b7e598e17ba73a
ramizkazak <ramizkazak@gmail.com>
2021-01-31 15:04:54 -0600

    Create us4 tc1 in ramiz branch 01

--- src/test/java/com/trycloud/tests/UserStrories/US_4_Talks_accessibility/Ramiz.java
+++ src/test/java/com/trycloud/tests/UserStrories/US_4_Talks_accessibility/Ramiz.java
@@ -1,4 +1,39 @@
 package com.trycloud.tests.UserStrories.US_4_Talks_accessibility;
 
-public class Ramiz {
+import com.trycloud.tests.base.TestBase;
+import com.trycloud.utilities.LoginUtil;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Ramiz extends TestBase {
+
+    @BeforeMethod
+    public void setupMethod(){
+        super.setUpMethod();
+    }
+    @Test(description="Test case #1 - verify users can access to Talks module")
+    public void accessToTalksModule(){
+
+        //1. Login as a user
+        LoginUtil.Login(driver,"userName1","password");
+
+        //2. Click Talks module
+        WebElement talkModule = driver.findElement(By.xpath("(//a[@aria-label='Talk'])[1]"));
+        talkModule.click();
+
+        //3. Verify the page tile is Talks module’s tile
+        WebElement currentTile = driver.findElement(By.xpath("(//a[@aria-label='Talk'])[1]"));
+        Assert.assertTrue(currentTile.isDisplayed());
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        super.tearDownMethod();
+    }
+
+
 }
