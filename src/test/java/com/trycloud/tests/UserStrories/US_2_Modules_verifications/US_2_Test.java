6c801ff98fcf8a9bb3b4435f06b066e2c9a974c9
Ahmed Sdiri <sdiri.ad@gmail.com>
2021-02-04 23:27:46 -0600

    created HomePage to store all menus in an arrayList created US_2 class

--- src/test/java/com/trycloud/tests/UserStrories/US_2_Modules_verifications/US_2_Test.java
+++ src/test/java/com/trycloud/tests/UserStrories/US_2_Modules_verifications/US_2_Test.java
@@ -1,13 +1,13 @@
 package com.trycloud.tests.UserStrories.US_2_Modules_verifications;
 
 import com.trycloud.tests.base.HomePage;
+import com.trycloud.utilities.ConfigurationReader;
 import com.trycloud.utilities.LoginUtil;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
-
 
 
 public class US_2_Test extends HomePage {
@@ -19,19 +19,22 @@ public class US_2_Test extends HomePage {
     }
 
     @Test
-    public void test(){
-       for (String eachUser: userNames) {
+    public void test() {
+        for (String eachUser : userNames) {
             LoginUtil.Login(driver, eachUser, "password");
-           System.out.println(eachUser+" test is running: !!");
-            for (String eachMenu: HomePage.listOfMenus) {
-                System.out.println(eachMenu);
-                Assert.assertTrue(getMenu(eachMenu).isDisplayed(), eachMenu+" was not found !!!!!!!!!!!!!!!!");
+            System.out.println("***********************");
+            System.out.println("Test using: " + ConfigurationReader.getProperty(eachUser) + " is running: !!");
+            for (String eachMenu : HomePage.listOfMenus) {
+                System.out.println(eachMenu + " ---  Found");
+                Assert.assertTrue(getMenu(eachMenu).isDisplayed(), eachMenu + " was not found !!!!!!!!!!!!!!!!");
             }
             LoginUtil.LogOut(driver);
-           System.err.println("Passed !!!!!!!!");
-           System.out.println("++++++++++++++");
+            System.err.println("+++++++!! Pass !!++++++");
+            System.out.println("***********************");
         }
+        System.err.println("**** -Test Passed- **** ");
     }
+
     @AfterMethod
     public void tearDownMethod() {
         super.tearDownMethod();
