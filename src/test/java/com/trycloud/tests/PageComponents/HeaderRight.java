26bac7fcb4162d86e0bab4b0c0196323a4aa91a9
Ahmed Sdiri <sdiri.ad@gmail.com>
2021-02-06 12:05:56 -0600

    Ahmed's Branch created App Setting class with the settings Method; CLeaned the HeaderLeft and HeaderRight.java

--- src/test/java/com/trycloud/tests/PageComponents/HeaderRight.java
+++ src/test/java/com/trycloud/tests/PageComponents/HeaderRight.java
@@ -1,38 +1,34 @@
 package com.trycloud.tests.PageComponents;
-
 import com.trycloud.tests.base.TestBase;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 public class HeaderRight extends TestBase {
+
     public static int headerRight(String menu) {
         int menuNumber = 0;
-        if (menu.equalsIgnoreCase("Magnify icon")) {
+        if (menu.equalsIgnoreCase("search")) {
             menuNumber = 1;
-        } else if (menu.equalsIgnoreCase("Notifications")) {
+        } else if (menu.equalsIgnoreCase("notifications")) {
             menuNumber = 2;
-        } else if (menu.equalsIgnoreCase("Contacts menu")) {
+        } else if (menu.equalsIgnoreCase("contacts")) {
             menuNumber = 3;
-        } else if (menu.equalsIgnoreCase("Settings menu")) {
+        } else if (menu.equalsIgnoreCase("Settings")) {
             menuNumber = 4;
         }
-        return menuNumber;
+        return menuNumber ;
+
     }
-    
-    
-    public static WebElement headerRightMenu(WebDriver driver,String menu) {
-        // 1.Magnify icon
+
+    public static WebElement headerRightMenu(WebDriver driver, String menu) {
+        // 1. search
         // 2.Notifications
-        // 3.Contacts menu
-        // 4.Settings menu
-        
-        
+        // 3.Contacts
+        // 4.Settings
         WebElement headerRight = driver.findElement(By.cssSelector("div[class='header-right']"));
-        return headerRight.findElement(By.xpath("//div[@class='header-right']/div[" + headerRight(menu)+ "]"));
-       
-        
-        
+        return headerRight.findElement(By.xpath("//div[@class='header-right']/div[" + headerRight(menu) + "]"));
     }
-    
+
 }
