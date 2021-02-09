87d67ad1166a19cfd0d682102939cb248196e895
ramizkazak <ramizkazak@gmail.com>
2021-02-09 01:50:17 -0600

    organized US4_Test Class

--- src/test/java/com/trycloud/tests/UserStrories/US_4_Talks_accessibility/Navigator.java
+++ src/test/java/com/trycloud/tests/UserStrories/US_4_Talks_accessibility/Navigator.java
@@ -3,11 +3,11 @@ package com.trycloud.tests.UserStrories.US_4_Talks_accessibility;
 import com.trycloud.utilities.BrowserUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.*;
-import org.testng.Assert;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class Navigator {
 
@@ -19,15 +19,12 @@ public class Navigator {
         PageFactory.initElements(driver, this);
     }
 
-    @FindBys({
-            @FindBy(how = How.XPATH, using = "//ul[@class='conversations']"),
-            @FindBy(how = How.XPATH, using = "//ul[@class='contacts-list']")
-    })
-    List<WebElement> allUsersInSearchResult;
 
+    @FindBy(xpath = "(//ul[@class='contacts-list']//li)")
+    public static List<WebElement> searchResult;
 
     @FindBy(how = How.XPATH, using = "//input[@class='app-navigation-search__input']")
-    WebElement searchInputBox;
+    public static WebElement searchInputBox;
 
     @FindBy(how = How.XPATH, using = "//a[@aria-label='Talk'])[1]")
     WebElement talksModule;
@@ -35,38 +32,40 @@ public class Navigator {
     @FindBy(how = How.XPATH, using = "//div[@class='new-message-form__advancedinput']")
     WebElement messageBoxInput;
 
+    @FindBy(xpath = "//button[@aria-label='Send message']")
+    WebElement messageSummitButton;
+
+
     @FindBy(how = How.XPATH, using = "(//div[@class='message-group'])[2]")
     List<WebElement> conversationMessageBox;
 
-    //Get title on current page
-    public String title() {
-        return driver.getTitle();
+    //Use Faker to create random message
+    public String autoMessage() {
+        return BrowserUtils.faker().backToTheFuture().quote();
     }
 
-    //Search User from searchBox and select, then send message method
+    //Search User from searchBox and select
     public void sendMessage(String personName) {
         searchInputBox.sendKeys(personName);
-        String message = BrowserUtils.faker().app().name();
+        BrowserUtils.sleep(3);
 
-        for (WebElement eachPerson : allUsersInSearchResult) {
-            if (eachPerson.getText().equalsIgnoreCase(personName)) {
-                eachPerson.click();
-                driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-                messageBoxInput.sendKeys(message);
-            } else {
-                System.out.println("The person, you want to start conversation, is not found in list.FAILED!!!");
-                driver.close();
-            }
+       // List<WebElement> resultList = searchResult;
+        for (WebElement eachResult : searchResult) {
+            if (eachResult.getText().equalsIgnoreCase(personName))
+                eachResult.click();
         }
 
-        for (WebElement eachConversation : conversationMessageBox) {
-            Assert.assertTrue(eachConversation.getText().equalsIgnoreCase(message));
-
-        }
     }
-
 }
 
 
+
+/*
+ for(int i =1; i<searchResult.size();i++){
+           if(searchResult.get(i).getText().contains(personName)){
+               searchResult.get(i).click();
+               messageBoxInput.sendKeys(BrowserUtils.faker().name().title());
+           }
+       }
+ */
 
--- src/test/java/com/trycloud/tests/UserStrories/US_4_Talks_accessibility/US_4_Test.java
+++ src/test/java/com/trycloud/tests/UserStrories/US_4_Talks_accessibility/US_4_Test.java
@@ -3,9 +3,13 @@ package com.trycloud.tests.UserStrories.US_4_Talks_accessibility;
 import com.trycloud.tests.PageComponents.HeaderLeft;
 import com.trycloud.tests.base.HomePage;
 import com.trycloud.utilities.LoginUtil;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class US_4_Test extends HomePage {
 
@@ -18,7 +22,6 @@ public class US_4_Test extends HomePage {
     @Test(description = "US4-Test case #1 - verify users can access to Talks module")
     public void US4_accessToTalkModule() {
 
-
         LoginUtil.Login(driver, "userName2", "password");   //login
 
         HeaderLeft.headerLeftMenu("spreed").click(); //go to talks module
@@ -26,25 +29,28 @@ public class US_4_Test extends HomePage {
         Assert.assertEquals(driver.getTitle(), "Talk - Trycloud QA", "Current page title is not match with expected result.FAILED!!!"); //verify the title
         LoginUtil.LogOut(driver); // close the driver
 
- /*
-      LoginUtil.Login(driver,"userName2","password");
-        Navigator talkModuleVerification = new Navigator(driver);
-        talkModuleVerification.talksModule.click();
-
-       String expectedTitle ="Talk - Trycloud QA";
-        Assert.assertEquals(talkModuleVerification.title(),expectedTitle,"Current page title is not match with expected result.FAILED!!!");
-        LoginUtil.LogOut(driver);
-      */
     }
 
     @Test(description = "US4-Test case #2 - verify users can send message")
     public void sendMessage() {
-        LoginUtil.Login(driver, "userName3", "password");
+        LoginUtil.Login(driver, "userName4", "password");
         Navigator sendMessageFunction = new Navigator(driver);
         HeaderLeft.headerLeftMenu("spreed").click();
 
-        sendMessageFunction.sendMessage("Employee100");
+        sendMessageFunction.sendMessage("user97");
 
+        String expectedMessage = "Test1234";
+        sendMessageFunction.messageBoxInput.sendKeys(expectedMessage);
+        sendMessageFunction.messageSummitButton.click();
+
+        List<String> actualMessage = new ArrayList<>();
+        for (WebElement each : sendMessageFunction.conversationMessageBox)
+            actualMessage.add(each.getText());
+
+        for (String each : actualMessage) {
+            Assert.assertTrue(each.contains(expectedMessage), "Expected mesage not contains in the actual message list.Failed!!!");
+        }
+        LoginUtil.LogOut(driver);
     }
 
 }
