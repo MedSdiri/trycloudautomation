e7b3c3b0835084046675925f7387cee4d4afba9d
Anna Shabalina <shabalina@gmail.com>
2021-02-08 23:30:45 -0500

    Anna`s 4th commit: -added ActivityPage (under Page Components).
    Merge remote-tracking branch 'origin/master'

--- src/test/java/com/trycloud/tests/UserStrories/US_5_Contacts_accessibility/Anna.java
+++ src/test/java/com/trycloud/tests/UserStrories/US_5_Contacts_accessibility/Anna.java
@@ -1,4 +1,23 @@
 package com.trycloud.tests.UserStrories.US_5_Contacts_accessibility;
 
-public class Anna {
+import com.trycloud.tests.base.TestBase;
+import org.testng.annotations.BeforeMethod;
+
+public class Anna extends TestBase {
+
+    @BeforeMethod
+    public void setUpMethod() {
+        super.setUpMethod();
+    }
 }
+
+
+
+
+
+/*
+5.Story: As a user, I should be able to access to Contacts module.
+Test case #1 - verify users can access to Talks module
+        1.Login as a user
+        2.Click “Contacts” module
+        3.Verify the page tile is Contents module’s*/
