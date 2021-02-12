3c1a82f87ab19683f0e87c117883888e52169922
ramizkazak <ramizkazak@gmail.com>
2021-02-11 19:25:22 -0600

    organized US4_Test Class

--- src/test/java/com/trycloud/tests/UserStrories/US_4_Talks_accessibility/US_4_Test.java
+++ src/test/java/com/trycloud/tests/UserStrories/US_4_Talks_accessibility/US_4_Test.java
@@ -3,22 +3,24 @@ package com.trycloud.tests.UserStrories.US_4_Talks_accessibility;
 import com.trycloud.tests.PageComponents.HeaderLeft;
 import com.trycloud.tests.base.HomePage;
 import com.trycloud.utilities.BrowserUtils;
-import com.trycloud.utilities.Driver;
 import com.trycloud.utilities.LoginUtil;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class US_4_Test extends HomePage {
-    Navigator talk = new Navigator();
+
 
     @BeforeMethod
-    public void setUp() {
-       setUpMethod();
+    public void setUpMethod() {
+        super.setUpMethod();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        super.tearDownMethod();
     }
 
     @Test(description = "US4-Test case #1 - verify users can access to Talks module")
@@ -33,34 +35,51 @@ public class US_4_Test extends HomePage {
 
     }
 
-
     @Test(description = "US4-Test case #2 - verify users can send message")
-    public void sendMessage() {
-        LoginUtil.Login(Driver.getDriver(), "userName4", "password");
-        String userName = "User84";
+    public void sendMessage_via_talks_module(){
 
+// create String of userName and message
+        String userName = "Mike India";
+        String message = "Hello India";
+
+        //login to page
+        LoginUtil.Login(driver, "userName4", "password");
+        Talks talk = new Talks();
+
+//click to talks module
         HeaderLeft.headerLeftMenu("spreed").click();
-
-        BrowserUtils.sleep(2);
-        talk.searchInputBox.sendKeys(userName);
-
-        for (WebElement eachUser : talk.searchResult) {
+        for (WebElement eachUser : talk.conversationList) {
+            BrowserUtils.sleep(1);
             if(eachUser.getText().contains(userName)){
                 eachUser.click();
+            }else {
+
+//click to create a new group conversation
+        talk.plusButton.click();
+        talk.conversationNameBox.sendKeys(userName);
+        talk.participantsButton.click();
+        talk.searchParticipantsBox.sendKeys(userName);
+        for(WebElement eachResult: talk.users){
+            if(eachResult.getText().equalsIgnoreCase(userName))
+                eachResult.click();
+        }
+        talk.createConversationButton.click();
             }
         }
 
-        talk.messageBoxInput.sendKeys("Hello World");
+//send a message to user
+        talk.messageBoxInput.sendKeys(message);
+        BrowserUtils.sleep(1);
         talk.messageSummitButton.click();
 
-        List<String> actualMessage = new ArrayList<>();
+//verify the message show on the conversation box
         for (WebElement each : talk.conversationMessageBox)
-            actualMessage.add(each.getText());
+            Assert.assertTrue(each.getText().contains(message), "Expected message not contains in the actual message list.Failed!!!");
 
-        for (String each : actualMessage) {
-            Assert.assertTrue(each.contains("Hello World"), "Expected mesage not contains in the actual message list.Failed!!!");
-        }
+//safety logOut the page
         LoginUtil.LogOut(driver);
+
+
     }
 
 }
